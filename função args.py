70755c8dd9268a83dcf001017dadcc167bc073d0
Arthur_Frantz <arhtursklT@hotmail.com>
2023-03-15 13:49:07 -0400

    paçoca2.0

--- função args.py
+++ função args.py
@@ -5,7 +5,7 @@ def world_cup_titles(country, *args):
 
 world_cup_titles('Brasil','1958','1962','1970','1994','2002','paçoca') 
 
-
+world_cup_titles.append(input())
 
 def concatena (**kwargs):
     print(f'')
