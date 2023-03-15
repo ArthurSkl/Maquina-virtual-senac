c9a0da996c0b57cc289b8477c3993938cf99d3bf
Arthur_Frantz <arhtursklT@hotmail.com>
2023-03-15 14:10:54 -0400

    varias coisa

--- app1.py
+++
@@ -1,9 +0,0 @@
-n=input("digite seu nome")
-i=int(input("digite sua idade "))
-a=float(input("digite sua altura "))
-
-def dados(n,i,a):
-    print("esses são seus dados cadrastrados no sistema !")
-    print("\nseu nome é ",n,"\nsua idade é ",i,"\nsua altura é de ",a)
-
-print(dados(n,i,a))
